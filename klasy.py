4b06d86ae9466083d65ab8de692618f2677ab198
dagmarahendigery <dagmara.hendigery@gmail.com>
2026-01-03 10:34:31 +0000

    Initial commit

---
+++ klasy.py
@@ -0,0 +1,12 @@
+class Czlowiek:
+    pass
+    def __init__(sel):
+        print("Niech powstanie czlowiek o imieniu {imie}")
+        self.imie = imie
+
+    adam = Czlowiek("Adam")
+    ewa = Czlowiek("Ewa")
+    print(adam.gatunek)
+    print(ewa.gatunek)
+    print(adam.imie)
+    print(adam.gatunek)
