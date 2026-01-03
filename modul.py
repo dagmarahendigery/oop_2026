3c4f8a4cbffa9cd228720349af8d564b36559cab
dagmarahendigery <dagmara.hendigery@gmail.com>
2026-01-03 13:01:58 +0000

    q
    def dodaj(a, b):
        return a + b
    
    def dodaj2(*args):
        print(args)
        print(type(args))
    
    args = (1, 2, 3, 4)
    
    wynik = dodaj2(*args)
    
    print(args)
    print(type(args))

--- modul.py
+++ modul.py
@@ -1,4 +1,19 @@
-def dodaj(a, b)
-    return a+b
+def dodaj(a, b):
+    return a + b
+
+
+def dodaj2(*args):
+    print(args)
+    print(type(args))
+
+
+args = (1, 2, 3, 4)
+
+wynik = dodaj2(*args)
+
+print(args)
+print(type(args))
+
+
 
 
